3446ad8878a54584923fc21d3a81c12999b7301e
lizen <lizen@bitsbeat.com>
2020-12-01 12:58:07 +0545

    setup node

---
+++ server-app/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.status(200).send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
